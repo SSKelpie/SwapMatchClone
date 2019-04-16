f0acf1a829236eab8471d11154f095e37dc3edc3
Gabi Dias <Gabi7778@hotmail.com>
2019-04-16 10:17:55 -0400

    Final Countdown
    -Matching & Counter code
    -perpetual screaming
    -heres most of my work

--- SwapMatchGridClone/Assets/Scripts/GridManager.cs
+++ SwapMatchGridClone/Assets/Scripts/GridManager.cs
@@ -5,111 +5,120 @@ using Vector3 = UnityEngine.Vector3;
 
 public class GridManager : MonoBehaviour
 {
-   // Start is called before the first frame update
-   const int rows = 7;
-   const int cols = 5;
-   int[,] _gems = new int[cols, rows];
-   private int _rainbow;
-   public Color[] color;
-   public List<Tile> gemObjects;
-   public GameObject Square;
-   public GameObject circle;
-   public PlayerMove pm;
-   
+    // Start is called before the first frame update
+    const int rows = 7;
+    const int cols = 5;
+    int[,] _gems = new int[cols, rows];
+    private int _rainbow;
+    public Color[] color;
+    public List<Tile> gemObjects;
+    public GameObject Square;
+    public GameObject circle;
+    public PlayerMove pm;
+    public int _matchCounter;
+
 //   public GameObject circle;
-   void Start()
-   {
-           gemObjects = new List<Tile>();
-
-       SetColor();
-       InstantiateGems();
-       Player();
-       pm = FindObjectOfType<PlayerMove>();
-       match();
-
-   }
-
-   void InstantiateGems()
-   {
-       for (int x = 0; x < cols; x++)
-       {
-           for (int y = 0; y < rows; y++)
-           {
-               Tile newtile = new Tile();
-               //Debug.Log("gem" + x + "," + y + ":" + _gems[x, y]);
-               GameObject gem = GameObject.Instantiate(Square);
-               newtile.prettygems = gem;
-               gem.transform.position = new Vector3(x, y);
-               newtile.location = gem.transform.position;
-               gem.GetComponent<SpriteRenderer>().color = color[Random.Range(0,5)];
-               gemObjects.Add(newtile);
-
-           }
-           
-       }
+    void Start()
+    {
+        gemObjects = new List<Tile>();
        
-       
-   }
 
-   void SetColor()
-   {
-       color[0] = UnityEngine.Color.red;
-       color[1] = UnityEngine.Color.yellow;
-       color[2] = UnityEngine.Color.blue;
-       color[3] = UnityEngine.Color.green;
-       color[4] = UnityEngine.Color.magenta;
-   }
+        SetColor();
+        InstantiateGems();
+        Player();
+        pm = FindObjectOfType<PlayerMove>();
+        
+    }
 
-   void Player()
-   {
-       GameObject player = GameObject.Instantiate(circle);
-       player.transform.position = new Vector3(2, 3);
-   }
+    void InstantiateGems()
+    {
+        for (int x = 0; x < cols; x++)
+        {
+            for (int y = 0; y < rows; y++)
+            {
+                Tile newtile = new Tile();
+                //Debug.Log("gem" + x + "," + y + ":" + _gems[x, y]);
+                GameObject gem = GameObject.Instantiate(Square);
+                newtile.prettygems = gem;
+                gem.transform.position = new Vector3(x, y);
+                newtile.location = gem.transform.position;
+                gem.GetComponent<SpriteRenderer>().color = color[Random.Range(0, 5)];
+                gemObjects.Add(newtile);
+
+            }
+
+        }
 
 
-   public GameObject Check4gameObject(Vector3 nextpos)
-   {
-       for (int i = 0; i < gemObjects.Count; i++)
-       {
-           if (nextpos == gemObjects[i].location)
-           {
-              
-               return gemObjects[i].prettygems;
-               
-           }
-           
-       }
+    }
 
-       return null;
-   }
-   
+    void SetColor()
+    {
+        color[0] = UnityEngine.Color.red;
+        color[1] = UnityEngine.Color.yellow;
+        color[2] = UnityEngine.Color.blue;
+        color[3] = UnityEngine.Color.green;
+        color[4] = UnityEngine.Color.magenta;
+    }
 
-   public void switchgem (GameObject switchObjects, Vector3 oldpos, Vector3 newpos)
-   {
-       switchObjects.transform.position = oldpos;
-       pm.transform.position = newpos;
-   }
-   // newpos = player stuff
+    void Player()
+    {
+        GameObject player = GameObject.Instantiate(circle);
+        player.transform.position = new Vector3(2, 3);
+    }
+
+
+    public GameObject Check4gameObject(Vector3 nextpos)
+    {
+        for (int i = 0; i < gemObjects.Count; i++)
+        {
+            if (nextpos == gemObjects[i].location)
+            {
+
+                return gemObjects[i].prettygems;
+
+            }
+
+        }
+
+        return null;
+    }
+
+
+    public void switchgem(GameObject switchObjects, Vector3 oldpos, Vector3 newpos)
+    {
+        switchObjects.transform.position = oldpos;
+        pm.transform.position = newpos;
+    }
+
+
+
+
+   // newposs = player stuff
    //oldpos = gameObjects
 
-   void match()
-   {
-       
-       for (int x = 0; x < cols; x++)
-       {
-           for (int y = 0; y < rows; y++)
-           {
-               if ((Vector2)gemObjects[x].location == new Vector2(x, y))
-               {
-                   SpriteRenderer checkColor = gemObjects[x].prettygems.GetComponent<SpriteRenderer>();
-                   checkColor.color = Color.red;
-                   
-                   Debug.Log("wtfman");
-               }
-           }
-
-       }
-   }
+//   void match();
+//   {
+//       
+//       for (int x = 0; x < cols; x++)
+//       {
+//           for (int y = 0; y < rows; y++)
+//           {
+//               if (x < cols - 2)
+//               {
+//                   Color checkColor1 = gemObjects[x].prettygems.GetComponent<SpriteRenderer>().color;
+//                   Color checkColor2 = gemObjects[y].prettygems.GetComponent<SpriteRenderer>().color;
+//                   checkColor1 = Color.red;
+//                   checkColor1 = Color.blue;
+//                   checkColor1 = Color.green;
+//                   checkColor1 = Color.yellow;
+//                   checkColor1 = Color.magenta;
+//                   
+//if (gemObjects[x - 1].prettygems )
+//    
+//              
+//               }
+// 
 }
 
 public class Tile
---
+++ SwapMatchGridClone/Assets/Scripts/checkingcollision.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class checkingcollision : MonoBehaviour
+{
+    public int type;
+    private bool topMatch, leftMatch, rightMatch, bottomMatch;
+    public GameObject mykids, topgem, leftgem, rightgem, bottomgem;
+    public SpriteRenderer matcha;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject mykids = transform.parent.gameObject;
+        Collider2D matcha = mykids.GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<Collider2D>())
+        { Debug.Log("ehhh");
+
+            Collider2D checkmatch = collision.gameObject.GetComponent<Collider2D>();
+            if (checkmatch.isActiveAndEnabled)
+            {
+                if (gameObject.name == "topCheck")
+                {
+                    GameObject.FindWithTag("top");
+                   
+                    if (checkmatch == matcha)
+                    {
+                        topMatch = true;
+                    }
+                    else
+                    {
+                        topMatch = false;
+                    }
+                }
+                else if (gameObject.name == "bottomCheck")
+                {
+                    GameObject.FindWithTag("bottom");
+                    if (checkmatch == matcha)
+                    {
+                        bottomMatch = true;
+                    }
+                    else
+                    {
+                        bottomMatch = false;
+                    }
+                }
+                else if (gameObject.name == "leftCheck")
+                {
+                    GameObject.FindWithTag("left");
+                    if (checkmatch == matcha)
+                    {
+                        leftMatch = true;
+                    }
+                    else
+                    {
+                        leftMatch = false;
+                    }
+                }
+                else if (gameObject.name == "rightCheck")
+                {
+                    GameObject.FindWithTag("right");
+                    if (checkmatch == matcha)
+                    {
+
+                        rightMatch = true;
+                    }
+                    else
+                    {
+                        rightMatch = false;
+                    }
+                }
+
+            }
+        }
+
+    }
+}
